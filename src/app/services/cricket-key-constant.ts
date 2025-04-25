c9a389bcac1c2b7649d238cb4d1f995d3d97b93b
Srinath <Srinathsiva500@gmail.com>
2025-04-25 13:06:31 +0530

    Status popup style changes

---
+++ src/app/services/cricket-key-constant.ts
@@ -0,0 +1,11 @@
+export class CricketKeyConstant {
+    status_code = {
+        success: '200',
+        refresh: '401',
+        refresh_msg:'Expired'
+    }
+    condition_key={
+        active_status:{key:'active','label':'Activate'},
+        deactive_status:{key:'deactive','label':'Deactivate'}
+    }
+}
