0bab7ac372e64b0b445c98a6e8b87787f295bbe0
Srinath <Srinathsiva500@gmail.com>
2025-04-30 17:28:39 +0530

    url constant file changes

--- src/app/services/url-constant.ts
+++ src/app/services/url-constant.ts
@@ -1,19 +1,29 @@
-export class URLCONSTANT{
+export class URLCONSTANT {
 
 
-           /*Country List*/
-    getCountryDropdown = "Country/get_country_dropdown"
-    addCountry = "Country/add_country"
-    editCountry = "Country/edit_country"
-    updateCountry = "Country/update_country"
-    viewCountry = "Country/view_country"
-    activeCountry = "Country/activate_country"
-    deactiveCountry ="Country/deactivate_country"
-    getCountryList ="Country/get_country_list"
+   /*Country List*/
+   getCountryDropdown = "Country/get_country_dropdown"
+   addCountry = "Country/add_country"
+   editCountry = "Country/edit_country"
+   updateCountry = "Country/update_country"
+   viewCountry = "Country/view_country"
+   activeCountry = "Country/activate_country"
+   deactiveCountry = "Country/deactivate_country"
+   getCountryList = "Country/get_country_list"
+
+   /*TEAM List*/
+   getTeamList = "Teams/get_teams_list"
+   activeTeam = "Teams/activate_team  "
+   deactiveTeam = "Teams/deactivate_team"
+   dropdownTeam = "Teams/get_teams_dropdown"
+   editTeam = "Teams/edit_team"
+   updateTeam = "Teams/update_team"
+   addTeam = "Teams/add_team"
+
+
+   /*PLAYERS REGISTRATION */
+   // getplayerlist = "Player/get_player_list"
 
 
 
-    playerList='assets/players.json';
-    officialList='assets/official.json';
-    compititionList='assets/compitition.json';
 }
