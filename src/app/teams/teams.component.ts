68ef67c6ebe2389b256e3f7dd133931a542bb8b8
Srinath <Srinathsiva500@gmail.com>
2025-04-30 16:37:55 +0530

    teams API implimentation

--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { BadgeModule } from 'primeng/badge';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DropdownModule } from 'primeng/dropdown';
 import { FileUploadModule } from 'primeng/fileupload';
 import { InputTextModule } from 'primeng/inputtext';
-import { SidebarModule } from 'primeng/sidebar';
+import { Sidebar, SidebarModule } from 'primeng/sidebar';
+import { ApiService } from '../services/api.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { URLCONSTANT } from '../services/url-constant';
+import { CricketKeyConstant } from '../services/cricket-key-constant';
+import { PaginatorModule } from 'primeng/paginator';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { DialogModule } from 'primeng/dialog';
+import { TagModule } from 'primeng/tag';
+import { EditTeam, Teams, UpdateTeams } from './teams.model';
 
 interface Team {
-  teamName: string;
-  ageGroup: string;
-  format: string;
-  country: string;
-  status: string;
+  config_id: string;
 }
 
 @Component({
@@ -25,148 +31,271 @@ interface Team {
     TableModule,
     ButtonModule,
     BadgeModule,
+    DialogModule,
     FormsModule,
     DropdownModule,
     FileUploadModule,
     InputTextModule,
-    SidebarModule
+    ReactiveFormsModule,
+    Sidebar,
+    PaginatorModule,
+    TagModule,
+    ConfirmDialogModule,
+    ToastModule
+   
   ],
   templateUrl: './teams.component.html',
-  styleUrls: ['./teams.component.css']
+  styleUrls: ['./teams.component.css'],
+  providers: [
+    { provide: URLCONSTANT },
+    { provide: CricketKeyConstant },
+    { provide: MessageService },
+    { provide: ConfirmationService }
+  ],
 })
-export class TeamsComponent {
-  teams: Team[] = [
-    {
-      teamName: 'India U19',
-      ageGroup: 'U-19',
-      format: 'ODI',
-      country: 'India',
-      status: 'Active'
-    },
-    {
-      teamName: 'Australia Seniors',
-      ageGroup: 'Senior',
-      format: 'Test',
-      country: 'Australia',
-      status: 'Inactive'
-    },
-    {
-      teamName: 'England T20',
-      ageGroup: 'Senior',
-      format: 'T20',
-      country: 'England',
-      status: 'Active'
-    }
-  ];
+export class TeamsComponent implements OnInit{
+  public addTeamForm!: FormGroup<any>;
 
-  ageGroups = [
-    { label: 'U-16', value: 'U-16' },
-    { label: 'U-19', value: 'U-19' },
-    { label: 'Senior', value: 'Senior' }
-  ];
+  user_id: number = Number(localStorage.getItem('user_id'));
+  client_id: number = Number(localStorage.getItem('client_id'));
+  public ShowForm: any = false;
+   teamData : Teams[] = [];
 
-  formats = [
-    { label: 'T20', value: 'T20' },
-    { label: 'ODI', value: 'ODI' },
-    { label: 'Test', value: 'Test' }
-  ];
+   configDataList: Team[] = [];
+   ageGroupList: Team[] = [];
+   genderList: Team[] = [];
+   formatList: Team[] = [];
 
-  countries = [
-    { label: 'India', value: 'India' },
-    { label: 'Australia', value: 'Australia' },
-    { label: 'England', value: 'England' },
-    { label: 'South Africa', value: 'South Africa' },
-    { label: 'New Zealand', value: 'New Zealand' }
-  ];
+   first: number = 1;
+   oldfirst: number = 1;
+   pageData: number = 0;
+   rows: number = 5;
+   totalData: any = 0;
+   submitted: boolean = true;
+   viewMode: boolean = false;
+   isEditMode: boolean = false;
 
-  countriesList = this.countries;
+   constructor(private formBuilder: FormBuilder, private apiService: ApiService, private urlConstant: URLCONSTANT, private msgService: MessageService,
+    private confirmationService: ConfirmationService,public cricketKeyConstant :CricketKeyConstant) {
 
-  displayModal = false;
-  isEditMode = false;
-  viewMode = false;
-  imagePreview: string | null = null;
-  selectedTeam: Team | null = null;
-  newTeams: Team = this.getEmptyTeam();
-nameRef: any;
+ }
+  ngOnInit(): void {
+   this.gridLoad()
+   this.getGlobalData()
+   this.addTeamForm = this.formBuilder.group({
+    team_id: [''],
+    team_name: ['', [Validators.required]],
+    team_short: ['', [Validators.required]],
+    gender_id: ['', [Validators.required]],
+    age_category_id: ['', [Validators.required]],
+    format_id: ['', [Validators.required]],
+    team_profile:['']
+  })
+  }
 
-  getEmptyTeam(): Team {
-    return {
-      teamName: '',
-      ageGroup: '',
-      format: '',
-      country: '',
-      status: 'Active'
+  gridLoad() {
+    const params: any = {};
+    params.user_id = this.user_id?.toString();
+    params.client_id = this.client_id?.toString();
+    params.page_no = this.first.toString();
+    params.records = this.rows.toString();
+    this.apiService.post(this.urlConstant.getTeamList, params).subscribe((res) => {
+      this.teamData = res.data.teams ?? [];
+      this.totalData = 50;
+      this.teamData.forEach((val: any) => {
+        val.profile_img = `${val.profile_img}?${Math.random()}`;
+      });
+    }, (err: any) => {
+           err.status === this.cricketKeyConstant.status_code.refresh && err.error.message === this.cricketKeyConstant.status_code.refresh_msg ? this.apiService.RefreshToken() : (this.teamData = [],this.totalData = this.teamData.length);
+
+    });
+
+  } 
+  
+  calculateFirst(): number {
+  return (this.first - 1) * this.rows;
+}
+onPageChange(event: any) {
+  this.first = (event.page) + 1;
+  this.pageData = event.first;
+  this.rows = event.rows;
+  this.gridLoad();
+}
+  showAddForm() {
+    this.ShowForm = true;
+  }
+
+  cancelForm() {
+    this.ShowForm = false;
+  }
+  resetForm() {
+
+    this.addTeamForm.reset();
+    this.submitted = false;
+  }
+  successToast(data: any) {
+    this.msgService.add({ key: 'tc', severity: 'success', summary: 'Success', detail: data.message });
+
+  }
+
+  /* Failed Toast */
+  failedToast(data: any) {
+    this.msgService.add({ key: 'tc', severity: 'error', summary: 'Error', detail: data.message });
+  }
+
+  status(team_id: number,url:string) {
+    const params: any = {
+      user_id: this.user_id?.toString(),
+      client_id: this.client_id?.toString(),
+      team_id: team_id?.toString()
     };
-  }
-
-  showAddTeamDialog(): void {
-    this.isEditMode = false;
-    this.viewMode = false;
-    this.displayModal = true;
-  }
-  addTeams(addAnother: boolean = false): void {
-    if (this.validateForm()) {
-      if (this.isEditMode) {
-        const index = this.teams.findIndex(t => t.teamName === this.selectedTeam?.teamName);
-        if (index !== -1) {
-          this.teams[index] = { ...this.newTeams };
-        }
-      } else {
-        this.teams.push({ ...this.newTeams });
+    this.apiService.post(url, params).subscribe(
+      (res:any) => {
+        res.status_code === this.cricketKeyConstant.status_code.success && res.status ? (this.successToast(res), this.gridLoad()) : this.failedToast(res);
+      },
+      (err:any) => {
+        err.status === this.cricketKeyConstant.status_code.refresh && err.error.message === this.cricketKeyConstant.status_code.refresh_msg ? this.apiService.RefreshToken() : this.failedToast(err);
       }
-
-      if (addAnother) {
-        
-        this.displayModal = true;
-        this.cancelForm();
-       // this.showAddTeamDialog();
-      } else {
-        this.displayModal = false;
+    );
+  }
+ 
+  StatusConfirm(team_id: number,actionObject:{key:string,label:string}) {
+    this.confirmationService.confirm({
+      message: `Are you sure you want to ${actionObject.label} this team?`,
+      header: 'Confirmation',
+      icon: 'pi pi-question-circle',
+      acceptLabel: 'Yes',
+      rejectLabel: 'No',
+      accept: () => {
+        const url:string= this.cricketKeyConstant.condition_key.active_status.key===actionObject.key?this.urlConstant.activeTeam:this.urlConstant.deactiveTeam;
+        this.status(team_id,url);
+        console.log(team_id);
+        this.confirmationService.close();
+      },
+      reject: () => {
+        this.confirmationService.close();
       }
-    }
-  }
-
-  validateForm(): boolean {
-    const t = this.newTeams;
-    if (!t.teamName || !t.ageGroup || !t.format || !t.country) {
-      return false;
-
-    }
-    return true;
-  }
-
-  clearForm(): void {
-    this.newTeams = this.getEmptyTeam();
-    this.imagePreview = null;
+    });
   }
   
-  cancelForm(): void {
-    this.displayModal = false;
-    this.clearForm();
+  addCallBack(res: any) {
+    this.resetForm();
+    this.cancelForm();
+    this.successToast(res);
+    this.gridLoad();
   }
+  getGlobalData() {
+    const params: any = {};
+    params.action_flag = 'dropdown';
+    params.user_id = this.user_id.toString();
+    params.client_id = this.client_id.toString();
+    this.apiService.post(this.urlConstant.dropdownTeam, params).subscribe((res) => {
+        this.configDataList = res.data.dropdowns != undefined ? res.data : [];
+        
+        this.ageGroupList = res.data.dropdowns
+        .filter((item: any) => item.config_key === 'age_category')
+        .map((item:any) => ({ ...item}));
+      
+        this.genderList = res.data.dropdowns
+        .filter((item: any) => item.config_key === 'gender')
+        .map((item:any) => ({ ...item}));
+      
+        this.formatList =res.data.dropdowns
+        .filter((item: any) => item.config_key === 'team_format')
+        .map((item:any) => ({ ...item}));
+      
+        setTimeout(() => {
+          const teamId = this.addTeamForm.get('team_id')?.value;
+          if (!teamId) {
+            this.formSetValue();
+          }
+        }, 100);
+        
+    }, (err: any) => {
+        if (err.status === 401 && err.error.message === "Expired") {
+            this.apiService.RefreshToken();
+           
+        }
+    })
+}
 
-  toggleStatus(team: Team): void {
-    team.status = team.status === 'Active' ? 'Inactive' : 'Active';
-  }
+  formSetValue(){
+    this.addTeamForm.patchValue({
+      gender_id: this.genderList[0].config_id,
+      format_id: this.formatList[0].config_id,
+      ageGroup: this.ageGroupList[0].config_id
+       })
+}
 
-  editTeams(team: Team): void {
-    this.isEditMode = true;
-    this.viewMode = false;
-    this.selectedTeam = team;
-    this.newTeams = { ...team };
-    this.displayModal = true;
-  }
 
-  viewTeams(team: Team): void {
-    this.isEditMode = false;
-    this.viewMode = true;
-    this.selectedTeam = team;
-    this.displayModal = true;
-  }
-
-  deleteTeams(index: number): void {
-    if (confirm('Are you sure you want to delete this team?')) {
-      this.teams.splice(index, 1);
+  onAddTeam() {
+    this.submitted = true;
+    if (this.addTeamForm.invalid) {
+      this.addTeamForm.markAllAsTouched();
+      return
+    }
+    const params: UpdateTeams = {
+      user_id: String(this.user_id),
+      client_id: String(this.client_id),
+      team_name: this.addTeamForm.value.team_name,
+      team_short: this.addTeamForm.value.team_short,
+      gender_id: String(this.addTeamForm.value.gender_id),
+      age_category_id: String(this.addTeamForm.value.age_category_id),
+      format_id: String(this.addTeamForm.value.format_id),
+      team_id: String(this.addTeamForm.value.team_id),
+      action_flag: 'create',
+    };
+    if (this.addTeamForm.value.team_id) {
+      params.action_flag = 'update';
+      this.apiService.post(this.urlConstant.updateTeam, params).subscribe((res) => {
+        res.status_code === this.cricketKeyConstant.status_code.success && res.status ? this.addCallBack(res) : this.failedToast(res);
+      }, (err: any) => {
+        err.status === this.cricketKeyConstant.status_code.refresh && err.error.message === this.cricketKeyConstant.status_code.refresh_msg ? this.apiService.RefreshToken() : this.failedToast(err);
+      });
+    } else {
+      this.apiService.post(this.urlConstant.addTeam, params).subscribe((res) => {
+        res.status_code === this.cricketKeyConstant.status_code.success && res.status ? this.addCallBack(res) : this.failedToast(res);
+      }, (err: any) => {
+        err.status === this.cricketKeyConstant.status_code.refresh && err.error.message === this.cricketKeyConstant.status_code.refresh_msg ? this.apiService.RefreshToken() : this.failedToast(err);
+      });
     }
   }
+
+
+  EditTeam(team_id: number) {
+    console.log("HIii",this.teamData)
+    const params: any = {};
+    params.user_id = this.user_id?.toString();
+    params.client_id  = this.client_id?.toString();
+    params.team_id = team_id?.toString();
+    this.apiService.post(this.urlConstant.editTeam, params).subscribe((res) => {
+      console.log(res);
+      if (res.status_code == 200) {
+        const editRecord: EditTeam = res.data.teams[0] ?? {};
+        if (editRecord != null) {
+          this.addTeamForm.setValue({
+            team_id: editRecord.team_id,
+            team_short: editRecord.team_short,
+            team_name: editRecord.team_name,
+            gender_id:editRecord.gender_id,
+            age_category_id: editRecord.age_category_id,
+            format_id: editRecord.format_id,
+            team_profile: null
+          });
+          this.showAddForm();
+        }
+      } else {
+        this.failedToast(res);
+      }
+    }, (err: any) => {
+      err.status === this.cricketKeyConstant.status_code.refresh && err.error.message === this.cricketKeyConstant.status_code.refresh_msg ? this.apiService.RefreshToken() : this.failedToast(err);
+    });
+
+
+  }
+
+
+
+
+
 }
---
+++ src/app/teams/teams.model.ts
@@ -0,0 +1,68 @@
+export class Teams {
+  team_id: number;
+  team_name: string;
+  gender: string;
+  age_category: string;
+  team_category: string;
+  record_status: string;
+  // time_zone_name: number;
+
+  constructor(item: Partial<Teams> = {}) {
+    this.team_id = item.team_id || 0;
+    this.team_name = item.team_name || '';
+    this.gender = item.gender || '';
+    this.age_category = item.age_category || '';
+    this.team_category = item.team_category || '';
+    this.record_status = item.record_status || '';
+
+  }
+}
+
+export class EditTeam {
+  team_id: number;
+  team_short: string;
+  team_name: string;
+  gender_id: number;
+  age_category_id: number;
+  format_id: number;
+  distrib_id: number;
+  //  team_profile: '';
+
+
+  constructor(item: Partial<EditTeam> = {}) {
+    this.team_id = item.team_id || 0;
+    this.team_short = item.team_short || '';
+    this.team_name = item.team_name || '';
+    this.gender_id = item.gender_id || 0;
+    this.age_category_id = item.age_category_id || 0;
+    this.format_id = item.format_id || 0;
+    this.distrib_id = item.distrib_id || 0;
+    // /this.team_profile = '';
+     }
+}
+
+export class UpdateTeams {
+    user_id: string|number;
+    client_id: string|number;
+    gender_id: string|number;
+    age_category_id: string|number;
+    format_id: string|number;
+    team_id: string|number;
+    team_name: string;
+    team_short: string;
+    action_flag: string;
+
+  
+    constructor(item: Partial<UpdateTeams> = {}) {
+      this.user_id = item.user_id || 0;
+      this.client_id = item.client_id || 0;
+      this.gender_id = item.gender_id || '';
+      this.team_name = item.team_name || '';
+      this.team_short = item.team_short || '';
+      this.age_category_id = item.age_category_id || 0;
+      this.format_id = item.format_id || 0;
+      this.team_id = item.team_id || 0;
+      this.action_flag = item.action_flag || '';
+
+    }
+  }
