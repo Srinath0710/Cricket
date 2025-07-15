3ff339749dd46ad327166c0dc280f603177c6865
dharani6817852 <dharanidharan.t@ckcet.ac.in>
2025-07-15 18:49:30 +0530

    country active popup changes

--- src/app/all-Component/country/country.component.ts
+++ src/app/all-Component/country/country.component.ts
@@ -501,32 +501,40 @@ export class CountryComponent implements OnInit {
     );
   }
 
-  StatusConfirm(country_id: number, actionObject: { key: string, label: string }, currentStatus: string) {
-    const AlreadyStatestatus =
-      (actionObject.key === this.conditionConstants.active_status.key && currentStatus === this.conditionConstants.active_status.status) ||
-      (actionObject.key === this.conditionConstants.deactive_status.key && currentStatus === this.conditionConstants.deactive_status.status);
+StatusConfirm(country_id: number, action: { key: string, label: string }, currentStatus: string) {
+    const { active_status, deactive_status } = this.conditionConstants;
+    const isSameStatus =
+      (action.key === active_status.key && currentStatus === active_status.status) ||
+      (action.key === deactive_status.key && currentStatus === deactive_status.status);
 
-    if (AlreadyStatestatus) {
-      return;
-    }
+    if (isSameStatus) return;
+
+    const isActivating = action.key === active_status.key;
+    const iconColor = isActivating ? '#4CAF50' : '#d32f2f'; 
+    const message = `Are you sure you want to proceed?`;
+
     this.confirmationService.confirm({
-      message: `Are you sure you want to ${actionObject.label} this country?`,
-      header: 'Confirmation',
-      icon: 'pi pi-question-circle',
+      header: ``,
+      message: `
+      <div class="custom-confirm-content">
+      <i class="fa-solid fa-triangle-exclamation warning-icon" style="color: ${iconColor};"></i>
+        <div class="warning">Warning</div>
+        <div class="message-text">${message}</div>
+      </div>
+    `,
       acceptLabel: 'Yes',
       rejectLabel: 'No',
+      styleClass: 'p-confirm-dialog-custom',
       accept: () => {
-        const url: string = this.conditionConstants.active_status.key === actionObject.key
-          ? this.urlConstant.activeCountry
-          : this.urlConstant.deactiveCountry;
+        const url = isActivating ? this.urlConstant.activeCountry : this.urlConstant.deactiveCountry;
         this.status(country_id, url);
         this.confirmationService.close();
       },
-      reject: () => {
-        this.confirmationService.close();
-      }
-    });
+      reject: () => this.confirmationService.close()
+    } as any);
   }
+
+
   filterGlobal() {
   if (this.searchKeyword.length >= 3 || this.searchKeyword.length === 0){
 
