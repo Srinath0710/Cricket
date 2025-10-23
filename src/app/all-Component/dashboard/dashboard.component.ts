fc7961b447cc32fedefc82ab1a46fb152a05eb29
dharani6817852 <dharanidharan.t@ckcet.ac.in>
2025-10-23 11:29:24 +0530

    tootltip label name changes for teams and grounds

--- src/app/all-Component/dashboard/dashboard.component.ts
+++ src/app/all-Component/dashboard/dashboard.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    CommonModule,
+    DropdownModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+  totalData: any = 0;
 }
